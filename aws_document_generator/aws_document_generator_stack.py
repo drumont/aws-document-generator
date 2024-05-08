63d41014f0fbf60decc9c322e394f5eb455cae27
Drumont <drumont06@gmail.com>
2024-05-08 13:56:26 +0200

    update main

--- aws_document_generator/aws_document_generator_stack.py
+++ aws_document_generator/aws_document_generator_stack.py
@@ -27,7 +27,7 @@ class AwsDocumentGeneratorStack(Stack):
         document_bucket = _s3.Bucket(
             self,
             "og-template-bucket",
-            #bucket_name="og-document-generator-bucket",
+            bucket_name="og-document-generator-bucket",
             access_control=_s3.BucketAccessControl.BUCKET_OWNER_FULL_CONTROL,
 
         )
@@ -39,12 +39,7 @@ class AwsDocumentGeneratorStack(Stack):
                 resources=[
                     document_bucket.bucket_arn,
                     document_bucket.arn_for_objects("*")
-                ],
-                conditions={
-                    "StringEquals": {
-                        "s3:DataAccessPointAccount": f"{Aws.ACCOUNT_ID}"
-                    }
-                }
+                ]
             )
         )
 
@@ -60,71 +55,7 @@ class AwsDocumentGeneratorStack(Stack):
             }
         )
 
-        # Object lambda s3 access
-        generate_pdf_function.add_to_role_policy(
-            _iam.PolicyStatement(
-                effect=_iam.Effect.ALLOW,
-                resources=["*"],
-                actions=["s3-object-lambda:WriteGetObjectResponse"]
-            )
-        )
-
-        # Restrict Lambda to be invoked from own account
-        generate_pdf_function.add_permission("invocationRestriction",
-                                             action="lambda:InvokeFunction",
-                                             principal=_iam.AccountRootPrincipal(),
-                                             source_account=Aws.ACCOUNT_ID)
-
-        # Associate Bucket's access point with lambda get access
-        if generate_pdf_function.role is not None:
-            policy_doc = _iam.PolicyDocument()
-            policy_statement = _iam.PolicyStatement(
-                effect=_iam.Effect.ALLOW,
-                actions=["s3:GetObject"],
-                principals=[
-                    _iam.ArnPrincipal(generate_pdf_function.role.role_arn)
-                ],
-                resources=[
-                   f"{self.access_point}/object/*"
-                ])
-
-        policy_statement.sid = "AllowLambdaToUseAccessPoint"
-        policy_doc.add_statements(policy_statement)
-
-        document_bucket_access_point = _s3.CfnAccessPoint(
-            self, "document_bucket_access_point",
-            bucket=document_bucket.bucket_name,
-            # name=S3_ACCESS_POINT_NAME,
-            policy=policy_doc
-        )
-
-        # Access point to receive GET request and use lambda to process objects
-        object_lambda_ap = _s3objectlambda.CfnAccessPoint(
-            self,
-            "document_bucket_access_point_object_lambda",
-            name=OBJECT_LAMBDA_ACCESS_POINT_NAME,
-            object_lambda_configuration=_s3objectlambda.CfnAccessPoint.ObjectLambdaConfigurationProperty(
-                supporting_access_point=self.access_point,
-                transformation_configurations=[
-                    _s3objectlambda.CfnAccessPoint.TransformationConfigurationProperty(
-                        actions=["GetObject"],
-                        content_transformation={
-                            "AwsLambda": {
-                                "FunctionArn": f"{generate_pdf_function.function_arn}"
-                            }
-                        }
-                    )
-                ]
-            )
-        )
-
-        CfnOutput(self, "exampleBucketArn", value=document_bucket.bucket_arn)
-        CfnOutput(self, "objectLambdaArn",
-                  value=generate_pdf_function.function_arn)
-        CfnOutput(self, "objectLambdaAccessPointArn", value=object_lambda_ap.attr_arn)
-        CfnOutput(self, "objectLambdaAccessPointUrl",
-                  value=f"https://console.aws.amazon.com/s3/olap/{Aws.ACCOUNT_ID}/"
-                        f"{OBJECT_LAMBDA_ACCESS_POINT_NAME}?region={Aws.REGION}")
+        document_bucket.grant_read_write(generate_pdf_function)
 
         api = _gateway.LambdaRestApi(
             self,
